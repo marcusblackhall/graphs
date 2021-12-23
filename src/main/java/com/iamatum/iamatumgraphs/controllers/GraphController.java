b15116f58c57eff28d5aff6b238ac7d915f29f57
Marcus Blackhall <marcusinamsterdam@gmail.com>
2021-12-23 17:38:54 +0100

    Set default landing page as home page

--- src/main/java/com/iamatum/iamatumgraphs/controllers/GraphController.java
+++ src/main/java/com/iamatum/iamatumgraphs/controllers/GraphController.java
@@ -8,6 +8,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class GraphController {
 
+    @RequestMapping("/")
+    public String homePage(Model model){
+       return showCards(model);
+    }
+
     @GetMapping("/home")
     public String showCards(Model model) {
         model.addAttribute("left", "card");
