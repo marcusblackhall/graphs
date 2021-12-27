81106358f3fb6d34ff56f1e9b397b2160a783f11
Marcus Blackhall <marcusinamsterdam@gmail.com>
2021-12-27 16:55:59 +0100

    Schedule log url corrected

--- src/main/java/com/iamatum/iamatumgraphs/rest/ScheduleLogRestController.java
+++ src/main/java/com/iamatum/iamatumgraphs/rest/ScheduleLogRestController.java
@@ -3,18 +3,20 @@ package com.iamatum.iamatumgraphs.rest;
 import com.iamatum.iamatumgraphs.domain.ScheduleLog;
 import com.iamatum.iamatumgraphs.services.ScheduleLogService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/log")
+@RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class ScheduleLogRestController {
 
     private final ScheduleLogService scheduleLogService;
 
+    @GetMapping("/log")
     public List<ScheduleLog> scheduleLog() {
         return scheduleLogService.retrieveScheduleLog();
     }
