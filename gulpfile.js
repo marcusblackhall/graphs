e203b1bac02b71638baee7c22e0f4fc9bb2c33e3
Marcus Blackhall <marcusinamsterdam@gmail.com>
2021-12-29 22:20:13 +0100

    Save gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+const gulp = require('gulp');
+//const sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
+const {series} = require('gulp');
+
+//compile scss into css
+function style() {
+    return gulp.src('custom/custom.scss')
+    .pipe(sass().on('error',sass.logError))
+    .pipe(gulp.dest('src/main/resources/static/css'));
+
+}
+
+function copybootstrap() {
+    return gulp.src('node_modules/bootstrap/dist/js/bootstrap.min.js')
+    .pipe(gulp.dest('src/main/resources/static/js'));
+
+}
+
+function copyjquery() {
+    return gulp.src('node_modules/jquery-ui/dist/jquery-ui.min.js')
+    .pipe(gulp.dest('src/main/resources/static/js'));
+
+}
+
+function copyjqueryui() {
+    return gulp.src('node_modules/jquery-ui/ui/widgets/datepicker.js')
+    .pipe(gulp.dest('src/main/resources/static/js'));
+
+}
+function copyjqueryuicss() {
+    return gulp.src('node_modules/jquery-ui/themes/base/datepicker.css')
+    .pipe(gulp.dest('src/main/resources/static/js'));
+
+}
+
+function copydatatables() {
+    return gulp.src('node_modules/datatables.net-dt/js/dataTables.dataTables.min.js')
+    .pipe(gulp.dest('src/main/resources/static/js'));
+
+}
+
+
+function copychartjs() {
+    return gulp.src('node_modules/chart.js/dist/chart.min.js')
+    .pipe(gulp.dest('src/main/resources/static/js'));
+
+}
+
+
+exports.copydatatables = copydatatables;
+exports.copyjqueryuicss = copyjqueryuicss;
+exports.copybootstrap = copybootstrap;
+exports.copychartjs = copychartjs;
+exports.copyjquery = copyjquery;
+exports.copyjqueryui = copyjqueryui;
+exports.style = style;
+exports.default = series(style,copybootstrap,copyjquery,copychartjs);
